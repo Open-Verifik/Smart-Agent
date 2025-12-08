fd7a41de4948b232c8fd8dee25f5638b9ae13cfc
migueltrevinom <miguel@verifik.co>
2025-12-08 12:02:17 -0500

    push logic for scrollable roadmap

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'three': ['three']
-        }
-      }
-    }
-  },
-  publicDir: 'public',
-  server: {
-    port: 3000
-  }
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					three: ["three"],
+				},
+			},
+		},
+	},
+	publicDir: "public",
+	server: {
+		port: 3000,
+	},
 });
-
