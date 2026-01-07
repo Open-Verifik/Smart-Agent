7e1d8a9c22354a2b4443cbbe7c907750b67fe9b6
migueltrevinom <miguel@verifik.co>
2026-01-07 17:53:28 -0500

    prod mode for the login of sms/whatsapp/email

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://staging-api.verifik.co',
+  baseUrl: 'https://prod.verifik.co',
   smartAgentUrl: 'https://x402-agent.verifik.co',
-  apiUrl: 'https://staging-api.verifik.co',
-  appUrl: 'https://staging-api.verifik.co',
+  apiUrl: 'https://prod.verifik.co',
+  appUrl: 'https://prod.verifik.co',
   projectId: '6266193db77ccc8111730c90',
   loginProjectFlowId: '658ed28b0990f300134d7b78',
 };
