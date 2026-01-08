041d37bd7df79467f878a276fc5b5acf80af46e4
migueltrevinom <miguel@verifik.co>
2026-01-08 15:46:05 -0500

    fix env project ids

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   smartAgentUrl: 'https://x402-agent.verifik.co',
   apiUrl: 'https://prod.verifik.co',
   appUrl: 'https://prod.verifik.co',
-  projectId: '6266193db77ccc8111730c90',
-  loginProjectFlowId: '658ed28b0990f300134d7b78',
+  projectId: '6332941ccde4f719d9c00f9e',
+  loginProjectFlowId: '6332941ccde4f719d9c00f9f',
 };
