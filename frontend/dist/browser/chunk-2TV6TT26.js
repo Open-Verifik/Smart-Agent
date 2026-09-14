c124e7013e7aa621c7378c1e80912d1ac8ad02dd
migueltrevinom <miguel@verifik.co>
2026-09-14 17:31:44 -0600

    fix issues related to reload balance on change language

---
+++ frontend/dist/browser/chunk-2TV6TT26.js
@@ -0,0 +1,1 @@
+import"./chunk-66YHNWRR.js";var e=[{path:"",redirectTo:"projects",pathMatch:"full"},{path:"plans",loadComponent:()=>import("./chunk-43TL7POU.js").then(t=>t.SmartAccessPlansComponent)},{path:"projects",loadChildren:()=>import("./chunk-FDLTC3VB.js").then(t=>t.default)}];export{e as default};
--- frontend/dist/browser/chunk-GYYN3E54.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-66YHNWRR.js";var t=[{path:"",loadComponent:()=>import("./chunk-JOREH3JG.js").then(o=>o.AddCreditsComponent)}];export{t as ADD_CREDITS_ROUTES};
